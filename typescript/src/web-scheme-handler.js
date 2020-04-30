ddff8ad40e7074359066d097184fb0b5e842e5f7
Callionica <16260283+callionica@users.noreply.github.com>
2020-04-30 08:26:05 +0100

    scheme handler

--- typescript/src/web-scheme-handler.js
+++ typescript/src/web-scheme-handler.js
@@ -1,10 +1,63 @@
 // ALL RIGHTS RESERVED
 
 // An implementation of WKURLSchemeHandler that gives more flexibility than the file:// scheme
-// when using WKWebView to read files from the local file system.
-
+// when using WKWebView to read files from the local file system. For example, using a custom
+// scheme allows us to provide a default document (index.html) when the URL represents a folder
 function WebSchemeHandler(config) {
-    let className = "CalliSchemeHandler";
+
+    // Get the file path and extension from a URL
+    // adding "/index.html" if the URL is a folder
+    function pathAndExtension(url) {
+        let path = url.path;
+        let extension = url.pathExtension;
+
+        const separator = "/";
+        function isFolder(url) {
+            return url.absoluteString.js.endsWith(separator);
+        }
+
+        if (isFolder(url)) {
+            extension = "html";
+            path = path + separator + "index.html";
+        }
+
+        return {path, extension};
+    }
+
+    function mimeTypeForExtension(extension) {
+        return "text/html"; // TODO
+    }
+
+    function WKURLSchemeHandler_webViewStartURLSchemeTask(webView, task) {
+        let url = task.request.URL;
+        let {path, extension} = pathAndExtension(url);
+        let mimeType = mimeTypeForExtension(extension);
+        let options = $.NSDataReadingMappedIfSafe;
+        let error = $();
+        let data = $.NSData.dataWithContentsOfFileOptionsError(path, options, error);
+        let expectedContentLength = data.length;
+
+        let response = $.NSURLResponse.alloc.initWithURLMIMETypeExpectedContentLengthTextEncodingName(url, mimeType, expectedContentLength, $());
+
+        /*
+        let headers = {
+            "Content-Type": mimeType,
+            "Cache-Control": "no-cache",
+        };
+
+        let httpResponse = $.NSHTTPURLResponse.alloc.initWithURLStatusCodeHTTPVersionHeaderFields(url, 200, $(), $(headers));
+        */
+
+        task.didReceiveResponse(response);
+        task.didReceiveData(data);
+        task.didFinish();
+    }
+
+    function WKURLSchemeHandler_webViewStopURLSchemeTask(webView, task) {
+        // Nothing to do here
+    }
+
+    let className = "CalliURLSchemeHandler";
     if (!$[className]) {
         ObjC.registerSubclass({
             name: className,
@@ -12,22 +65,16 @@ function WebSchemeHandler(config) {
             methods: {
                 'webView:startURLSchemeTask:': {
                     types: ["void", ["id", "id"]],
-                    implementation: function (webView, task){
-                        let url = task.request.url.copy;
-                        url.scheme = "file";
-
-                        let 
-                    }
+                    implementation: WKURLSchemeHandler_webViewStartURLSchemeTask,
                 },
                 'webView:stopURLSchemeTask:': {
                     types: ["void", ["id", "id"]],
-                    implementation: function(webView, task){
-
-                    }
+                    implementation: WKURLSchemeHandler_webViewStopURLSchemeTask,
                 }
             }
         });
     }
+
     let handler = $[className].alloc.init;
     return handler;
 }
