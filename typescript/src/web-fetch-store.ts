5bca591d78655a04c135906e6cbf8bd28bb58c15
Callionica <16260283+callionica@users.noreply.github.com>
2020-05-14 15:21:14 +0100

    FetchStore basics

--- typescript/src/web-fetch-store.ts
+++ typescript/src/web-fetch-store.ts
@@ -26,25 +26,41 @@ type FetchStoreResult = {
 };
 
 function createDirectory(path: string) {
-	var d = $.NSDictionary.alloc.init;
-	var url = $.NSURL.alloc.initFileURLWithPath(path);
-	$.NSFileManager.defaultManager.createDirectoryAtURLWithIntermediateDirectoriesAttributesError(url, true, d, null);
+    var d = $.NSDictionary.alloc.init;
+    var url = $.NSURL.alloc.initFileURLWithPath(path);
+    $.NSFileManager.defaultManager.createDirectoryAtURLWithIntermediateDirectoriesAttributesError(url, true, d, null);
 }
 
-function readJSON(path: string): any {
-    let data = $.NSFileManager.defaultManager.contentsAtPath(path); // NSData
-    let contents = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
-    if (contents.isNil()) {
-        contents = $.NSString.alloc.initWithDataEncoding(data, $.NSWindowsCP1252StringEncoding);
+// Read a UTF8 file and parse it as JSON
+// Any failures are ignored and the default value is returned instead
+function readJSON(path: string, _default: any = {}): any {
+    try {
+        let data = $.NSFileManager.defaultManager.contentsAtPath(path); // NSData
+        let contents = $.NSString.alloc.initWithDataEncoding(data, $.NSUTF8StringEncoding);
+        if (contents.isNil()) {
+            contents = $.NSString.alloc.initWithDataEncoding(data, $.NSWindowsCP1252StringEncoding);
+        }
+        if (contents.isNil()) {
+            contents = $.NSString.alloc.initWithDataEncoding(data, $.NSMacOSRomanStringEncoding);
+        }
+        return JSON.parse(contents.js);
+    } catch (e) {
     }
-    if (contents.isNil()) {
-        contents = $.NSString.alloc.initWithDataEncoding(data, $.NSMacOSRomanStringEncoding);
-    }
-    return JSON.parse(contents.js);
+    return _default;
 }
+
+function writeJSON(path: string, value: any) {
+    let json = JSON.stringify(value, null, 2);
+    let error = $();
+    $(json).writeToFileAtomicallyEncodingError(path, true, $.NSUTF8StringEncoding, error);
+}
+
+//type NSFetchReturn = {data: any, response: any};
 
 class FetchStore {
     path: string;
+
+    session: any;
 
     constructor(path: string) {
         if (!path.endsWith("/")) {
@@ -53,6 +69,55 @@ class FetchStore {
         createDirectory(path);
 
         this.path = path;
+
+        let cache = $.NSURLCache.sharedURLCache;
+
+        function createSession(cache: any) {
+            let configuration = $.NSURLSessionConfiguration.defaultSessionConfiguration;
+            configuration.waitsForConnectivity = true
+            configuration.URLCache = cache;
+            return $.NSURLSession.sessionWithConfiguration(configuration);
+        }
+
+        this.session = createSession(cache);
+    }
+
+    // NS implementation
+    async fetch_(url: any, dataPath: string): Promise<FetchStoreResult> {
+        function createDataTask(session: any, url: any, handler: any) {
+            let policy = $.NSURLRequestUseProtocolCachePolicy;
+            let timeout = 60.0; // seconds
+            let request = $.NSURLRequest.requestWithURLCachePolicyTimeoutInterval(url, policy, timeout);
+            return session.dataTaskWithRequestCompletionHandler(request, handler);
+        }
+
+        function getHeaders(response: any) {
+            let headers = { ...(response.allHeaderFields.js) };
+            for (let [key, value] of Object.entries(headers)) {
+                headers[key] = ObjC.unwrap(value);
+            }
+            return headers;
+        }
+
+        let promise = new Promise<FetchStoreResult>((resolve, reject) => {
+            function handler(data: any, response: any, error: any) {
+                if (!error.isNil()) {
+                    reject(error);
+                } else {
+                    data.writeToFileAtomically(dataPath, true);
+                    let headers = getHeaders(response);
+                    writeJSON(dataPath + ".headers", headers);
+                    let retrievalDate = new Date();
+                    let result = { path: dataPath, headers, retrievalDate };
+                    resolve(result);
+                }
+            }
+
+            let dataTask = createDataTask(this.session, url, handler);
+            dataTask.resume;
+        });
+
+        return promise;
     }
 
     async fetchStore(url: string, policy: FetchStorePolicy = FetchStorePolicy24): Promise<FetchStoreResult> {
@@ -75,31 +140,50 @@ class FetchStore {
         let path = this.path + `${group}${item}`;
         createDirectory(path);
 
-        let extension = (nsurl.pathExtension.length == 0) ? ".data" : `.${nsurl.pathExtension.js}`;
-        let dataPath = path + `data${extension}`;
-        let headersPath = path + `headers.txt`;
-        
+        let extension = (nsurl.pathExtension.length == 0) ? "data" : `${nsurl.pathExtension.js}`;
+        let dataPath = path + `data.${extension}`;
+        let headersPath = dataPath + ".headers";
+
+        let makeRequest = true;
+        let returnLater = true;
+
+        let result;
+
         let dataExists = $.NSFileManager.defaultManager.fileExistsAtPath(dataPath);
-
-        try {
-            let error = $();
-            let modificationDate;
-            let attrs = $.NSFileManager.defaultManager.attributesOfItemAtPathError(dataPath, error);
-            if (attrs && error.isNil()) {
-                modificationDate = attrs.fileModificationDate;
-            }
-
-            let headers = { "Content-Type": "text/plain; charset=utf-8" };
+        if (dataExists) {
             try {
-                headers = readJSON(headersPath);
-            } catch (e) {
-            }
+                let error = $();
+                let retrievalDate: Date = new Date("2001-01-01");
+                let attrs = $.NSFileManager.defaultManager.attributesOfItemAtPathError(dataPath, error);
+                if (attrs && error.isNil()) {
+                    retrievalDate = attrs.fileModificationDate.js;
 
-            return { path: dataPath, headers, retrievalDate: modificationDate.js };
-        } catch (e) {
+                    let now = new Date();
+                    let age = now.valueOf() - retrievalDate.valueOf();
+                    makeRequest = age >= policy.refreshIfOlderThan;
+                    returnLater = age >= policy.returnIfYoungerThan;
+                }
+
+                if (!returnLater) {
+                    let headers = readJSON(headersPath, { "Content-Type": "text/plain; charset=utf-8" });
+                    result = { path: dataPath, headers, retrievalDate };
+                }
+            } catch (e) {
+                makeRequest = true;
+                returnLater = true;
+            }
         }
-    
-        return { path: dataPath, headers: {}, retrievalDate: new Date() };
-        
+
+        let fetchPromise: Promise<FetchStoreResult> = Promise.reject(new Error("Unexpected"));
+
+        if (makeRequest) {
+            fetchPromise = this.fetch_(nsurl, dataPath);
+        }
+
+        if (!returnLater && result) {
+            return result;
+        }
+
+        return fetchPromise;
     }
 }
