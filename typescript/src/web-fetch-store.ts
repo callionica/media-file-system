4fb3f91b64f3d235efa3b467064b513d075e3d6c
Callionica <16260283+callionica@users.noreply.github.com>
2020-05-14 11:28:49 +0100

    WIP

--- typescript/src/web-fetch-store.ts
+++ typescript/src/web-fetch-store.ts
@@ -2,26 +2,33 @@
 // Unlike a typical HTTP cache, it doesn't follow the server's instructions about caching.
 // Caching is entirely under the control of the client.
 // Documents that are retrieved from a web server through the FetchStore will be saved permanently.
-// Documents in the FetchStore can be retrieved even if no network connection is available.
+// Documents in the FetchStore can be retrieved even if no network connection is available
+// and even if the connection is available, but the server returns an error.
 
-type FetchStoreAge = number; // seconds
+type FetchStoreAge = number; // milliseconds
 
 type FetchStorePolicy = {
     returnIfYoungerThan: FetchStoreAge;
     refreshIfOlderThan: FetchStoreAge;
 };
 
-// Returns stored content if downloaded in last 24 hours
-// otherwise contacts the server and returns the newly downloaded data
+// Return stored content if downloaded in last 24 hours
+// otherwise contact the server and return the newly downloaded data
 const FetchStorePolicy24 = {
-    returnIfYoungerThan: 24 * 60 * 60,
-    refreshIfOlderThan: 24 * 60 * 60,
+    returnIfYoungerThan: 24 * 60 * 60 * 1000,
+    refreshIfOlderThan: 24 * 60 * 60 * 1000,
 };
 
 type FetchStoreResult = {
     path: string;
     retrievalDate: Date;
 };
+
+function createDirectory(path: string) {
+	var d = $.NSDictionary.alloc.init;
+	var url = $.NSURL.alloc.initFileURLWithPath(path);
+	$.NSFileManager.defaultManager.createDirectoryAtURLWithIntermediateDirectoriesAttributesError(url, true, d, null);
+}
 
 class FetchStore {
     path: string;
@@ -30,13 +37,34 @@ class FetchStore {
         if (!path.endsWith("/")) {
             path += "/";
         }
-
+        createDirectory(path);
+        
         this.path = path;
     }
 
     async fetchStore(url: string, policy: FetchStorePolicy = FetchStorePolicy24): Promise<FetchStoreResult> {
-        let dataPath = this.path + "data/";
-        let metadataPath = this.path + "metadata/";;
+        // TODO - handle queries
+        let nsurl = $.NSURL.URLWithString(url);
+        let group = `${nsurl.scheme.js}/${nsurl.host.js}`;
+        let item = `${nsurl.path.js}`;
+        if (!item.startsWith("/")) {
+            item = "/" + item;
+        }
+
+        if (item === "/") {
+            item = "/(root)";
+        }
+
+        if (!item.endsWith("/")) {
+            item += "/";
+        }
+
+        let path = this.path + `${group}${item}`;
+        createDirectory(path);
+
+        let extension = nsurl.pathExtension.isNil() ? ".data" : `.${nsurl.pathExtension.js}`;
+        let dataPath = path + `data${extension}`;
+        let metadataPath = path + `metadata.txt`;
         return { path: "", retrievalDate: new Date() };
     }
 }
