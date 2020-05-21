4cab39abfcbebbe0e21af8c3ab795b4c7de0a3fc
Callionica <16260283+callionica@users.noreply.github.com>
2020-05-21 16:49:57 +0100

    show commands

--- typescript/src/client/keyboard.ts
+++ typescript/src/client/keyboard.ts
@@ -1,11 +1,13 @@
 class KeyboardCommand {
     name: string;
     shortcut: string;
+    enabled: boolean;
     action: (shortcut: string) => boolean;
 
     constructor(name: string, shortcut: string, action: (shortcut: string) => boolean) {
         this.name = name;
         this.shortcut = shortcut;
+        this.enabled = true;
         this.action = action;
     }
 }
@@ -15,21 +17,58 @@ function toShortcut(event: KeyboardEvent) {
     let control = event.ctrlKey ? "^" : "";
     let alt = event.altKey ? "⌥" : "";
     let shift = event.shiftKey ? "⇧" : "";
-    let key = event.key;
+    let key = ["Meta", "Control", "Alt", "Shift"].includes(event.key) ? "" : event.key;
     return `${command}${control}${alt}${shift}${key}`;
 }
 
 class KeyboardController {
     commands: KeyboardCommand[] = [];
 
-    route(event: KeyboardEvent) {
+    commandsVisible: boolean = false;
+    commandsVisibleTimeout: any = undefined;
+
+    hideCommands() {
+        clearTimeout(this.commandsVisibleTimeout);
+        this.commandsVisibleTimeout = undefined;
+
+        if (this.commandsVisible) {
+            this.commandsVisible = false;
+            console.log("hide commands");
+            return true;
+        }
+        return false;
+    }
+
+    showCommands() {
+        if (!this.commandsVisible && this.commandsVisibleTimeout == undefined) {
+            this.commandsVisibleTimeout = setTimeout(() => {
+                this.commandsVisibleTimeout = undefined;
+                this.commandsVisible = true;
+                console.log("Commands:", JSON.stringify(this.commands, null, 2));
+            }, 2 * 1000);
+        }
+    }
+
+    onkeydown(event: KeyboardEvent) {
         let shortcut = toShortcut(event);
-        let commands = this.commands.filter(command => command.shortcut === shortcut);
-        let handled = false;
-        for (let command of commands) {
-            handled = command.action(shortcut);
-            if (handled) {
-                break;
+        console.log(shortcut, event);
+
+        let handled = this.hideCommands();
+
+        if (!handled) {
+            if (shortcut == "⌘") {
+                this.showCommands();
+                return;
+            }
+
+            let commands = this.commands.filter(command => command.enabled && (command.shortcut === shortcut));
+
+            handled = false;
+            for (let command of commands) {
+                handled = command.action(shortcut);
+                if (handled) {
+                    break;
+                }
             }
         }
 
@@ -37,5 +76,12 @@ class KeyboardController {
             event.stopPropagation();
             event.preventDefault();
         }
+    }
+
+    onkeyup(event: KeyboardEvent) {
+        let shortcut = toShortcut(event);
+        console.log(shortcut, event);
+
+        this.hideCommands();
     }
 }
